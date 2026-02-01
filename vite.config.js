ff6845ce7ef1b7d095bc616901718976f57d6b5f
Dmytro Serhiienko <conneciones@gmail.com>
2026-02-01 22:31:10 +0200

    work

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/gsap-mojito-practice/", // Додаємо цей рядок
   plugins: [react(), tailwindcss()],
-})
+});
